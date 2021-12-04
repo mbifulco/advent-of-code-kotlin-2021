85a0427c7f2dd206a19b1ad096aaad23d712e312
Mike Bifulco <mbifulco@live.com>
2021-12-04 09:50:19 -0500

    day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -31,8 +31,67 @@ fun main() {
     return Integer.parseInt(gamma,2) * Integer.parseInt(epsilon,2)
   }
 
+  fun oxygenGeneratorRating(input: List<String>) : Int {
+    var zeroes:List<String> = emptyList();
+    var ones:List<String> = emptyList();
+
+    var remaining = input;
+
+    var i:Int = 0
+    while (i < input[0].length && remaining.size > 1) {
+      for (instruction in remaining) {
+        if (instruction.get(i).compareTo('0') == 0) {
+          zeroes += instruction
+        } else {
+          ones += instruction
+        }
+      }
+
+      if (zeroes.size > ones.size) {
+        remaining = zeroes;
+      } else {
+        remaining = ones;
+      }
+      i += 1
+      zeroes = emptyList()
+      ones = emptyList()
+    }
+
+    return Integer.parseInt(remaining[0],2);
+  }
+
+  fun co2ScrubberRating(input: List<String>):Int {
+    var zeroes:List<String> = emptyList();
+    var ones:List<String> = emptyList();
+
+    var remaining = input;
+
+    var i:Int = 0
+    while (i < input[0].length && remaining.size > 1) {
+      for (instruction in remaining) {
+        if (instruction.get(i).compareTo('0') == 0) {
+          zeroes += instruction
+        } else {
+          ones += instruction
+        }
+      }
+
+      if (zeroes.size <= ones.size) {
+        remaining = zeroes;
+      } else {
+        remaining = ones;
+      }
+      i += 1
+      zeroes = emptyList()
+      ones = emptyList()
+    }
+
+    println(remaining.size)
+    return Integer.parseInt(remaining[0],2);  
+  }
+
   fun part2(input: List<String>): Int {
-    return input.size
+    return oxygenGeneratorRating(input) * co2ScrubberRating(input);
   }
 
   val input = readInput("Day03")
